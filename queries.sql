97e7fd0ab17167a8543bfd0130ca38d25ffe596a
Fombi Favour <fombifavour@gmail.com>
2023-10-23 19:01:17 +0100

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -6,3 +6,31 @@ SELECT name, escape_attempts FROM animals WHERE weight_kg>10.5;
 SELECT * FROM animals WHERE neutered=true;
 SELECT * FROM animals WHERE NOT name='Gabumon';
 SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
+
+-- adding "species" in animal table
+BEGIN;
+UPDATE animals SET species='unspecified';
+SELECT * FROM animals;
+ROLLBACK;
+
+BEGIN;
+UPDATE animals SET species='digimon' WHERE name LIKE '%mon';
+UPDATE animals SET species='pokemon' WHERE species IS NULL;
+COMMIT;
+SELECT * FROM animals;
+
+BEGIN;
+DELETE FROM animals;
+SELECT * FROM animals;
+ROLLBACK;
+SELECT * FROM animals;
+
+BEGIN;
+DELETE FROM animals WHERE date_of_birth > '01-01-2022';
+SAVEPOINT savepoint_1;
+UPDATE animals set weight_kg=weight_kg * -1;
+SELECT * FROM animals;
+ROLLBACK;
+UPDATE animals SET weight_kg=weight_kg * -1 WHERE weight_kg < 0;
+COMMIT;
+SELECT * FROM animals;
