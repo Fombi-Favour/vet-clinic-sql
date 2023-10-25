f11fb4fb0355ab674a6933d12add3d1c5eb70a9c
Fombi Favour <fombifavour@gmail.com>
2023-10-25 12:33:34 +0100

    Update data.sql

--- data.sql
+++ data.sql
@@ -25,8 +25,8 @@ INSERT INTO species (name) VALUES('Pokemon');
 INSERT INTO species (name) VALUES('Digimon');
 
 -- updating data in "animals" table
-UPDATE animals SET species_id=2 WHERE name LIKE '%mon';
-UPDATE animals SET species_id=1 WHERE species_id IS NULL;
+UPDATE animals SET species_id = (SELECT id FROM species WHERE name = 'Digimon') WHERE name LIKE '%mon';
+UPDATE animals SET species_id = (SELECT id FROM species WHERE name = 'Pokemon') WHERE species_id IS NULL;
 
 UPDATE animals SET owner_id=(SELECT id from owners WHERE full_name='Sam Smith') WHERE name='Agumon';
 UPDATE animals SET owner_id=(SELECT id from owners WHERE full_name='Jennifer Orwell') WHERE name IN ('Gabumon', 'Pikachu');
